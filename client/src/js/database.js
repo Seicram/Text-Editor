97597e1c9968281eb6843f50977a0014649458b8
marcies smith <marcies.smith@gmail.com>
2023-06-09 14:36:15 -0400

    fixing code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
+const initdb = async () => {
+  return openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -11,11 +11,28 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.put({ content });
+  await tx.complete;
+  console.log('Content stored in IndexedDB');
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const data = await store.getAll();
+  await tx.complete;
+  if (data && data.length > 0) {
+    console.log('Content retrieved from IndexedDB');
+    return data[data.length - 1].content;
+  }
+  return null;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt; // Allows to show the install prompt
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault(); // Prevent the default prompt
+    deferredPrompt = event; // Store the event for later use
+    // Show your custom install button or UI
+    butInstall.style.display = 'block';
+  });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt(); // Show the prompt to install the PWA
+      const choiceResult = await deferredPrompt.userChoice;
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the PWA installation');
+      } else {
+        console.log('User dismissed the PWA installation');
+      }
+      deferredPrompt = null; // Reset the deferred prompt event
+      // Hide your custom install button or UI
+      butInstall.style.display = 'none';
+    }
+  });
+  
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA installed successfully');
+  });
